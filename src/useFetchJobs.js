2a7364f21c0d7f4b4b340ac0de25fca202f68887
jessicamhunt <j.m.hunt1018@gmail.com>
2020-10-03 21:13:31 -0400

    Actions and reducers to fetch jobs from github api.

--- src/useFetchJobs.js
+++ src/useFetchJobs.js
@@ -1,7 +1,48 @@
+import { useReducer, useEffect } from "react";
+import axios from "axios";
+
+const ACTIONS = {
+  MAKE_REQUESTS: "MAKE_REQUESTS",
+  GET_DATA: "GET_DATA",
+  ERROR: "ERROR",
+};
+
+const BASE_URL =
+  "https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json";
+
+function reducer(state, action) {
+  switch (action.type) {
+    case ACTIONS.MAKE_REQUESTS:
+      return { loading: true, jobs: [] };
+    case ACTIONS.GET_DATA:
+      return { ...state, loading: false, jobs: action.payload.jobs };
+    case ACTIONS.ERROR:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload.error,
+        jobs: [],
+      };
+    default:
+      return state;
+  }
+}
+
 export default function useFetchJobs(params, page) {
-  return {
-    jobs: [],
-    loading: false,
-    error: false,
-  };
+  const [state, dispatch] = useReducer(reducer, { jobs: [], loading: true });
+
+  useEffect(() => {
+    dispatch({ type: ACTIONS.MAKE_REQUESTS });
+    axios
+      .get(BASE_URL, {
+        params: { markdown: true, page: page, ...params },
+      })
+      .then((res) => {
+        dispatch({ type: ACTIONS.GET_DATA, payload: { jobs: res.data } });
+      })
+      .catch((e) => {
+        dispatch({ type: ACTIONS.ERROR, payload: { error: e } });
+      });
+  }, [params, page]);
+  return state;
 }
